df469e1e44eadde016d857fdc2ac1c551220a080
djtal <guillaume.garcera@gmail.com>
2015-11-03 19:41:45 +0100

    End of chapter 8

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,11 +1,29 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  title: '',
+  songCreationStarted: false,
+
+  isAddButtonDisabled: Ember.computed('title', function(){
+    return Ember.isEmpty(this.get('title'));
+  }),
+
+  noSongs: Ember.computed('model.songs.length', function() {
+    return this.get('model.songs.length') === 0;
+  }),
+
+  canCreateSong: Ember.computed('songCreationStarted', 'model.songs.length', function() {
+    return this.get('songCreationStarted') || this.get('model.songs.length');
+  }),
+
   actions: {
     updateRating: function(params){
       var song = params.item,
           rating = params.rating;
       song.set('rating', rating);
-    }
+    },
+    enableSongCreation: function() {
+      this.set('songCreationStarted', true);
+    },
   }
 });
