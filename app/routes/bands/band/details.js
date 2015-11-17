f34e7bb782a035ef4ce2a02a0f9fd951fe7b2274
djtal <guillaume.garcera@gmail.com>
2015-11-17 19:06:46 +0100

    End chapter 10

--- app/routes/bands/band/details.js
+++ app/routes/bands/band/details.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
+    save: function() {
+      var controller = this.get('controller'),
+      band = controller.get('model');
+      return band.save();
+    },
     willTransition: function(transition) {
       var controller = this.get('controller'), leave;
       if (controller.get('isEditing')) {
