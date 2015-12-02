280c0b18dc763a9a5b918dfcdee6b1b6f4ec7dba
djtal <guillaume.garcera@gmail.com>
2015-12-02 19:24:43 +0100

    End of Chapter 12

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -1,8 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  queryParams: {
+    sortBy: 'sort',
+    searchTerm: 's'
+  },
   title: '',
   songCreationStarted: false,
+  searchTerm: '',
+  sortBy: 'ratingDesc',
+
+  sortProperties: Ember.computed('sortBy', function() {
+    var options = {
+      "ratingDesc": "rating:desc,title:asc",
+      "ratingAsc": "rating:asc,title:asc",
+      "titleDesc": "title:desc",
+      "titleAsc": "title:asc",
+    };
+    return options[this.get('sortBy')].split(',');
+  }),
+
+  matchingSongs: Ember.computed('model.songs.@each.title', 'searchTerm', function() {
+    var searchTerm = this.get('searchTerm').toLowerCase();
+    return this.get('model.songs').filter(function(song) {
+      return song.get('title').toLowerCase().indexOf(searchTerm) !== -1;
+    });
+  }),
+
+  sortedSongs: Ember.computed.sort('matchingSongs', 'sortProperties'),
 
   isAddButtonDisabled: Ember.computed('title', function(){
     return Ember.isEmpty(this.get('title'));
